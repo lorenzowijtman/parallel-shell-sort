54c4bbcaa76eeaa89496d524a9aadb0b07fb62dc
Lorenzo <lorenzowijtman@hotmail.com>
2020-11-28 15:26:54 +0100

    first commit

---
+++ src/es/ua/dtic/rebbitmq_comsumer_se/Main.java
@@ -0,0 +1,9 @@
+package es.ua.dtic.rebbitmq_comsumer_se;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("hello");
+    }
+}
