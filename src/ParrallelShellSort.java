14af172fc690d09ff50a1186a0319ef3de6ce316
Walter <wtesevic@gmail.com>
2020-12-09 15:32:20 +0100

    parralel sort (maybe good)

--- src/ParrallelShellSort.java
+++ src/ParrallelShellSort.java
@@ -1,56 +1,76 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class ParrallelShellSort {
-    static int[] gaps = {701, 301, 132, 57, 23, 10, 4, 1};
+    // Volatile list to work with concurrency.
+    private volatile int[] sorted = {};
 
-    volatile int[] sorted = {};
+   public int[] sort(int[] list) {
+       this.sorted = list;
 
-    final int MAX_T = 4;
+       // Put 4 threads in a list. These threads handle the comparisons of the indices.
+       //
+       ArrayList<ShellComparator> threads = new ArrayList<>(Arrays.asList(
+               new ShellComparator("t1", 0),
+               new ShellComparator("t2", 1),
+               new ShellComparator("t3", 2),
+               new ShellComparator("t4", 3)));
 
+       ExecutorService pool;
 
-    public int[] sort(int[] list) {
-        this.sorted = list;
+       for (int i = threads.size(); i >= 1; i--) {
+           pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors()); // walter's pc = 4, lorenzo's = 8
 
-        // decide the interval
-        int interval = 3;
+           List<Callable<Object>> tasks = new ArrayList<>();
 
-        Runnable t1 = new ShellComparator("t1",0, interval);
-        Runnable t2 = new ShellComparator("t2", 1, interval);
-        Runnable t3 = new ShellComparator("t3",2, interval);
+           for (int k = 0; k < i; k++) {
+               threads.get(k).gap = i;
+               tasks.add(Executors.callable(threads.get(k)));
+           }
 
-        ExecutorService pool = Executors.newFixedThreadPool(MAX_T);
+           try {
+               System.out.println("begin");
+               pool.invokeAll(tasks);
+               System.out.println("end");
+           } catch (InterruptedException e) {
+               e.printStackTrace();
+           }
+       }
 
-        pool.execute(t1);
-        pool.execute(t2);
-        pool.execute(t3);
-
-        pool.shutdown();
-
-        return this.sorted;
-    }
-
+       return this.sorted;
+   }
 
     class ShellComparator implements Runnable {
 
-        private String name;
-        private int startIndex;
-        private int gap;
+    private String name;
+    private int startIndex;
 
-        ShellComparator(String name, int startIndex, int gap) {
-            this.name = name;
-            this.startIndex = startIndex;
-            this.gap = gap;
-        }
+    public int gap;
 
-        @Override
-        public void run() {
-            int prevNumber = sorted[startIndex];
-            for (int i = startIndex ; i < sorted.length; i += gap) {
-                sorted[startIndex]
-            }
-        }
+    ShellComparator(String name, int startIndex) {
+        this.name = name;
+        this.startIndex = startIndex;
     }
 
+    @Override
+    public void run() {
+        for (int i = startIndex ; i < sorted.length - gap; i += gap) {
+            int j = sorted[i];
+            int k = sorted[i + gap];
+
+            if (this.name.equals("t1")) System.out.println("J: " + j + '\n' + "K: "  + k);
+
+            if (j > k) {
+                sorted[i + gap ] = j;
+                sorted[i] = k;
+           }
+        }
+
+    }
+}
 }
